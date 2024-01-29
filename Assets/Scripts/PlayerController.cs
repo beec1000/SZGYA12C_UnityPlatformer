734cc4ac9dbc9ca074e81ee01df168c16f4c88bc
beec1000 <beec1000.minecraft@gmail.com>
2024-01-29 11:21:27 +0100

    Added Missile with Smoke Trail

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,53 @@
-using Unity.Mathematics;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float maxSpeed = 7;
-    private Rigidbody2D rigidbody2d;
-    private Animator animator;
-    private bool isFacingRight;
+    [SerializeField] private float jumpHeight = 375;
 
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    private Animator animator;
+    private Rigidbody2D rigidbody2d;
+    private bool isFacingRight;
+    private bool isGrounded;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         isFacingRight = true;
-
+        isGrounded = false;
+    }
+    private void Update()
+    {
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            isGrounded = false;
+            animator.SetBool("isGrounded", false);
+            rigidbody2d.AddForce(new(0, jumpHeight));
+        }
     }
 
     private void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
         animator.SetFloat("speed", Mathf.Abs(move));
-
-        rigidbody2d.velocity = new Vector2(
-            x: move * maxSpeed,
-            y: rigidbody2d.velocity.y);
-
+        rigidbody2d.velocity = new(move * maxSpeed, rigidbody2d.velocity.y);
         if ((move < 0 && isFacingRight) || (move > 0 && !isFacingRight)) Flip();
 
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, .15f, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("verticalSpeed", rigidbody2d.velocity.y);
     }
-
     private void Flip()
     {
         isFacingRight = !isFacingRight;
-        transform.localScale = new Vector3(
-            x: transform.localScale.x * -1,
-            y: transform.localScale.y);
-
-
+        transform.localScale = new(
+            x:transform.localScale.x * -1,
+            y: transform.localScale.y,
+            z: transform.localScale.z);
     }
 }
